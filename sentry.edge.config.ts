47e4f217e56f9bf518334ede6f061bf012b1d190
raad <raadbouks@gmail.com>
2024-06-01 17:14:01 +0000

    update

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -6,7 +6,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://7b883d075caa19b41fd9b00ae313a1c6@o4506813739368448.ingest.us.sentry.io/4507222371729408",
+  dsn: "https://32b13efff26e376a711374bcd216c77e@o4507357497262080.ingest.us.sentry.io/4507357507026944",
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
